b55218b6e08ed0210f9e811d710cf871e82f7f81
sebagalan <sebastiangalan@gmail.com>
2011-04-04 20:22:36 -0300

    Explota cuando llama la funcion Trampolin...todavia no se por que

--- src/project1/src/geekos/elf.c
+++ src/project1/src/geekos/elf.c
@@ -31,12 +31,15 @@ int Parse_ELF_Executable(char *exeFileData, ulong_t exeFileLength,
     struct Exe_Format *exeFormat)
 {
 
+	int i=0, offset=0;
+	
 	elfHeader *elfh = NULL;
 	programHeader *ph = NULL;
-	
+	struct Exe_Segment *s = NULL;
+
 	exeFormat = Malloc(sizeof(struct Exe_Format));
 	KASSERT(exeFormat != NULL);
-	memset(exeFormat,0,sizeof(struct Exe_Format));
+	memset(exeFormat,'\0',sizeof(struct Exe_Format));
 	KASSERT(exeFormat != NULL);
 	
 	elfh = Malloc(sizeof(elfHeader));
@@ -44,15 +47,9 @@ int Parse_ELF_Executable(char *exeFileData, ulong_t exeFileLength,
 	memset(elfh,0,sizeof(elfHeader));
 	KASSERT(elfh != NULL);
 
-	ph = Malloc(sizeof(programHeader));
-	KASSERT(ph != NULL);
-	memset(ph,0,sizeof(programHeader));
-	KASSERT(ph != NULL);
-
-	memcpy(elfh,exeFileData,sizeof(elfHeader));
+	elfh = memcpy(elfh,exeFileData,sizeof(elfHeader));
 	
-	Print("file size %ul\n",exeFileLength);
-	
+/*	Print("file size %u\n",exeFileLength);
 	Print("%s\n",elfh	->	ident);
 	Print("elfh ->	type %i\n",elfh	->	type);
 	Print("elfh ->	machine %i\n",elfh	->	machine);
@@ -66,24 +63,39 @@ int Parse_ELF_Executable(char *exeFileData, ulong_t exeFileLength,
 	Print("elfh ->	phnum %i\n",elfh	->	phnum);
 	Print("elfh ->	shentsize %i\n",elfh	->	shentsize);
 	Print("elfh ->	shnum %i\n",elfh	->	shnum);
-	Print("elfh ->	shstrndx %i\n",elfh	->	shstrndx);
+	Print("elfh ->	shstrndx %i\n",elfh	->	shstrndx);*/
 
-	memcpy(ph,exeFileData+elfh->phoff,(elfh->phnum*elfh->phentsize));
+	offset = elfh->phoff;
 
-	Print("ph ->	type %u\n",ph -> type);
-    Print("ph ->	offset %u\n",ph -> offset);
-    Print("ph ->	vaddr %u\n",ph -> vaddr);
-    Print("ph ->	paddr %u\n",ph -> paddr);
-    Print("ph ->	fileSize %u\n",ph -> fileSize);
-    Print("ph ->	memSize %u\n",ph -> memSize);
-    Print("ph ->	flags %u\n",ph -> flags);
-    Print("ph ->	alignment %u\n",ph -> alignment);
+	for(i=0;i< elfh->phnum; i++){
 
-	Free(elfh);
-	Free(ph);
-
+		ph =  (programHeader *)(exeFileData+offset);
+		Print("ph->type %u ",ph -> type);
+		Print("ph->offset %u ",ph -> offset);
+		Print("ph->vaddr %u ",ph -> vaddr);
+		Print("ph->paddr %u",ph -> paddr);
+		Print("ph->fileSize %u",ph -> fileSize);
+		Print("ph->memSize %u",ph -> memSize);
+		Print("ph->flags %u",ph -> flags);
+		Print("ph->alignment %u\n",ph -> alignment);
+	
+		s = Malloc(sizeof(struct Exe_Segment));
+		memset(s,0,sizeof(struct Exe_Segment));
+		
+		s->offsetInFile= ph->offset;	 
+		s->lengthInFile = ph->fileSize;	 
+		s->startAddress = ph->vaddr;
+		s->sizeInMemory = ph->memSize;
+		s->protFlags = ph -> flags;
+		exeFormat->segmentList[i] = *s;
+		offset = offset+32;
+	}
+	
+	exeFormat->numSegments = elfh->phnum ;
+	exeFormat->entryAddr = elfh->entry;	 	
+	/*No elimino s, porque quiero usarlo mas tarde...*/
     /*TODO("Parse an ELF executable image");*/
 
-	return -1;
+	return 0;
 }
 
