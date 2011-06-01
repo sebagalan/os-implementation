5a70aa2e11f20607f435fbecdcf6450a00edbf2a
sebagalan <sebastiangalan@gmail.com>
2011-06-01 01:26:48 -0300

    correr procesos en background

--- src/project3/src/user/shell.c
+++ src/project3/src/user/shell.c
@@ -275,9 +275,19 @@ void Spawn_Single_Command(struct Process procList[], int nproc, const char *path
     if (pid < 0)
 	Print("Could not spawn process: %s\n", Get_Error_String(pid));
     else {
-	int exitCode = Wait(pid);
-	if (exitCodes)
-	    Print("Exit code was %d\n", exitCode);
+        int len_command = strlen(procList[0].command);
+        int i = 0;
+
+        for( i = len_command ; i > 0 ; --i){
+            if(procList[0].command[i] == '&')
+                break;
+        }
+
+         if(i == 0){
+             int exitCode = Wait(pid);
+             if (exitCodes)
+                 Print("Exit code was %d\n", exitCode);
+         }
     }
 }
 
